bbfe6958734a15bd6cd4d87f73392bc6d03699c2
ABDUL <abdulhadi85430@gmail.com>
2024-01-17 02:18:59 +0530

    first commit

---
+++ project/src/components/Footer.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+const Footer = () => {
+    return (
+        <>
+            <section class="footer">
+                <div class="share">
+                    <a href="https://github.com/Abdul-4u" class="fab fa-facebook-f"></a>
+
+                    <a href="#" class="fab fa-instagram"></a>
+                    <a href="https://www.linkedin.com/in/abdul-hadi-rayni-2b0372233/" class="fab fa-linkedin"></a>
+
+                </div>
+                <div class="links">
+                    <a href="#">home</a>
+                    <a href="#">about</a>
+                    <a href="#">menu</a>
+                    <a href="#">products</a>
+                    <a href="#">review</a>
+                    <a href="#">contact</a>
+                    <a href="#">blogs</a>
+                </div>
+
+            </section>
+        </>
+    );
+};
+
+export default Footer;
---
+++ project/src/components/Home.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+const Home = () => {
+    return (
+        <>
+            <section className="home" id="home">
+                <div className="content">
+                    <h3>
+                        <span>food at your </span><br />Place
+                    </h3>
+                    <p className="desc">
+                        Pahle पेट pooza fir kaam दूजा
+                    </p>
+                    <a href="#" className="btn">
+                        get yours now
+                    </a>
+                </div>
+            </section >
+        </>
+    );
+};
+
+export default Home;
